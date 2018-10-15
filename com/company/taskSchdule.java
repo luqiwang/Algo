25bf422da349b3bf0a7dfc70d4914263af2e7e6d
luqiwang <wangluqi1001@gmail.com>
2018-10-15 12:53:24 -0400

    interval is small and type has many

--- com/company/taskSchdule.java
+++ com/company/taskSchdule.java
@@ -4,9 +4,10 @@ import java.util.*;
 
 public class taskSchdule {
     public static void main(String[] args) {
-        int[] tasks = new int[]{1,2,1,2,3,2,4};
-        int n = 3;
+        int[] tasks = new int[]{1,2,1,2,3,2,2,4,5,5};
+        int n = 2;
         System.out.println(taskSchdule1(tasks, n));
+        System.out.println(taskSchdule2(tasks, n));
     }
     // in original order
     public static int taskSchdule1(int[] tasks, int n) {
@@ -33,8 +34,38 @@ public class taskSchdule {
         System.out.println(Arrays.toString(arr));
         return time;
     }
+    // in original order, if cooldown time is small and tasks type are large.
+    // Worst Time: O(nk) n : tasks.length k: interval
+    // Ex: [1,1,1,1,1,1,1,1,1,1], interval = 2;
+    public static int taskSchdule2(int[] tasks, int n) {
+        List<Integer> ans = new ArrayList<>();
+        int time = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        for (int task : tasks) {
+            while (queue.contains(task)) {
+                time++;
+                ans.add(-1);
+                queue.offer(-1);
+                if (queue.size() > n) {
+                    int out = queue.poll();
+                    if (out == task) break;
+                }
+            }
+            queue.offer(task);
+            if (queue.size() > n) {
+                queue.poll();
+            }
+            time++;
+            ans.add(task);
+        }
+        int[] arr = ans.stream().mapToInt(i->i).toArray();
+        System.out.println(Arrays.toString(arr));
+        return time;
+    }
+
+
     // Output one of the final task schedule without order with cool time
-    public static List<Character> taskSchdule12(char[] tasks, int n) {
+    public static List<Character> taskSchdule3(char[] tasks, int n) {
         List<Character> ans = new ArrayList<>();
         Map<Character, Integer> map = new HashMap<>();
         for (char c : tasks) {
